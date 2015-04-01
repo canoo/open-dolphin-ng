73a2d0cf8b555511b33bbac73fc9644b8d6247b1
Sven Ehrke <sven.ehrke@canoo.com>
2015-04-01 17:23:08 +0200

    gulp task 'buildjs' with jshint and minification/uglifying js is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglifyjs');
+var notify = require('gulp-notify');
 
-gulp.task('scripts', function() {
-  return gulp.src('src/*.js')
-	.pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-	.pipe(gulp.dest('gulp_out'))
-	;
+gulp.task('buildjs', function () {
+	return gulp.src('src/*.js')
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('default'))
+		.pipe(gulp.dest('gulp_out'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('gulp_out'))
+		.pipe(notify({ message: 'buildjs task complete' }))
+		;
 });
