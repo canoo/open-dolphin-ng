a9189a769a3ae1ec22d86485450afc8cc5bad145
Sven Ehrke <sven.ehrke@canoo.com>
2015-04-03 00:30:14 +0200

    using 'gulp-ng-annotate' so that minified version works as well

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,14 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglifyjs');
 var sourcemaps = require('gulp-sourcemaps');
 var notify = require('gulp-notify');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('buildjs', function () {
 	return gulp.src('src/*.js')
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('default'))
 		.pipe(gulp.dest('gulp_out'))
+		.pipe(ngAnnotate())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(uglify())
 		.pipe(gulp.dest('gulp_out'))
