f87ff25e857f33ed9b109e21512e28e4f4062a85
Sven Ehrke <sven.ehrke@canoo.com>
2015-04-02 15:43:39 +0200

    getting gradle task 'clean' to take dir 'gulp_out' into account

--- buildSrc/src/main/groovy/org.opendolphin.gradle/GulpTask.groovy
+++ buildSrc/src/main/groovy/org.opendolphin.gradle/GulpTask.groovy
@@ -2,12 +2,21 @@ package org.opendolphin.gradle
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 public class GulpTask extends DefaultTask {
 
 	@Input
 	List<String> commands
+
+	@OutputDirectory
+	File destinationDir = new File('gulp_out')
+
+	GulpTask() {
+		destinationDir = new File('gulp_out')
+		println destinationDir.absolutePath
+	}
 
 	@TaskAction
 	void callGulp() {
